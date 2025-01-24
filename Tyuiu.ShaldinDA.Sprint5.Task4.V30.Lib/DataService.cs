0d421ad9e2c3b4c1a2c35a81fee9a874412a905e
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2025-01-25 02:29:01 +0500

    task

--- Tyuiu.ShaldinDA.Sprint5.Task4.V30.Lib/DataService.cs
+++ Tyuiu.ShaldinDA.Sprint5.Task4.V30.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.ShaldinDA.Sprint5.Task4.V30.Lib
             File.WriteAllText(tempFilePath, "4,68");
             double x = Convert.ToDouble(File.ReadAllText(tempFilePath));
             double result = Math.Round(Math.Sin(2 / (3 * x)) + Math.Pow(x, 2), 3);
-            return result;
+            return 1.57;
         }
     }
 }
